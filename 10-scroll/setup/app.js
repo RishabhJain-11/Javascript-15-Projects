28c625934cde1a129080c72840d607d6278298b8
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-25 12:11:58 +0530

    ..

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -11,23 +11,57 @@ const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
-navToggle.addEventListener('click', function(){
+navToggle.addEventListener('click', function () {
     // linksContainer.classList.toggle('show-links');
     const containerHeight = linksContainer.getBoundingClientRect().height;
 
     const linksHeight = links.getBoundingClientRect().height;
 
-    if(containerHeight === 0){
+    if (containerHeight === 0) {
         linksContainer.style.height = `${linksHeight}px`
-    }else{
+    } else {
         linksContainer.style.height = 0;
     }
-    
+
 })
+
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 // ********** fixed navbar ************
+window.addEventListener('scroll', function () {
+    // console.log(window.pageYOffset);
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
 
-
-
+    if (scrollHeight > navHeight) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        //prevent default
+        e.preventDefault();
+
+        //navigate to specific path
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        let position = element.offsetTop;
+        window.scrollTo({
+            left: 0, 
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    })
+})
