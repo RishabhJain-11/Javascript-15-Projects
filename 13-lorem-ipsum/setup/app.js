d203639ea636059a8870184251e6b80843b21b00
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-29 18:18:06 +0530

    ..

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -16,17 +16,23 @@ const form = document.querySelector('.lorem-form');
 const amount = document.getElementById('amount');
 const result = document.querySelector('.lorem-text');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
   e.preventDefault();
   const value = parseInt(amount.value);
-  // console.log(value);
+  const random = Math.floor(Math.random() * text.length);
 
   //empty
   //-1
   // >9
 
-  if(isNaN(value) || value < 0 || value > 9){
-    result.innerHTML = `<p class="result">${text[0]}</p>`;
+  if (isNaN(value) || value < 0 || value > 9) {
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
+  }
+  else{
+    let tempText = text.slice(0, value);
+    tempText = tempText.map(function(item){
+      return `<p class="result">{item}</p>`
+    })
   }
 
 });
